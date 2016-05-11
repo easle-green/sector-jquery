b5e5ac8fedf2e6710520ae3159ca20ebd868f649
trofa13 <trofa13@inbox.ru>
2016-05-11 09:23:55 +0300

    request callback fn have to be checked

--- assets/scripts/request-callback.js
+++ assets/scripts/request-callback.js
@@ -1,30 +1,30 @@
 (function(SECTOR){
-	SECTOR.requestHandler = {
-		response: null,
-		handleRequest: function (url, callback) {
-		var xhr = new XMLHttpRequest(),
-		self = this;
-		xhr.open('GET', url);
-		xhr.responseType = 'json';
-		xhr.onloadend = function() {
-			if(xhr.readyState === 4 && xhr.status === 200){
-				self.response = xhr.response;
-				callback(xhr.response);
-			}
-		};
-		if (!self.response){
-			xhr.send();
-			console.log('sending request');
-		} else {
-			callback(self.response);
-			console.log('not sending request');
-			return;
-		}	
-	},
+  SECTOR.requestHandler = {
+    response: null,
+    handleRequest: function (url, callback) {
+    var xhr = new XMLHttpRequest(),
+    self = this;
+    xhr.open('GET', url);
+    xhr.responseType = 'json';
+    xhr.onloadend = function() {
+      if(xhr.readyState === 4 && xhr.status === 200){
+        self.response = xhr.response;
+        callback(xhr.response);
+      }
+    };
+    if (!self.response){
+      xhr.send();
+      console.log('sending request');
+    } else {
+      callback(self.response);
+      console.log('not sending request');
+      return;
+    } 
+  },
 
-	updateResponse: function(){
-		SECTOR.requestHandler.response = null;
-	}
+  updateResponse: function(){
+    SECTOR.requestHandler.response = null;
+  }
 };
 
 setInterval(SECTOR.requestHandler.updateResponse, 5000);
