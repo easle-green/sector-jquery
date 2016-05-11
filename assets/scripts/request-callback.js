0d67b8eecad59d1ddca42bbc1c090f59a59a29ec
trofa13 <trofa13@inbox.ru>
2016-05-11 09:22:50 +0300

    request callback fn have to be checked

---
+++ assets/scripts/request-callback.js
@@ -0,0 +1,32 @@
+(function(SECTOR){
+	SECTOR.requestHandler = {
+		response: null,
+		handleRequest: function (url, callback) {
+		var xhr = new XMLHttpRequest(),
+		self = this;
+		xhr.open('GET', url);
+		xhr.responseType = 'json';
+		xhr.onloadend = function() {
+			if(xhr.readyState === 4 && xhr.status === 200){
+				self.response = xhr.response;
+				callback(xhr.response);
+			}
+		};
+		if (!self.response){
+			xhr.send();
+			console.log('sending request');
+		} else {
+			callback(self.response);
+			console.log('not sending request');
+			return;
+		}	
+	},
+
+	updateResponse: function(){
+		SECTOR.requestHandler.response = null;
+	}
+};
+
+setInterval(SECTOR.requestHandler.updateResponse, 5000);
+
+})(window.SECTOR);
--- assets/scripts/updateStatus.js
+++ assets/scripts/updateStatus.js
@@ -1,46 +1,45 @@
 (function(SECTOR){
 
 SECTOR.updateState = {
-	currentChannel: document.getElementById('track-loader').dataset.channel,
+    currentChannel: document.getElementById('track-loader').dataset.channel,
 
-	request: function(callback){
-		var xhr = new XMLHttpRequest(),
-				self = this;
-		xhr.open('GET', 'http://sectorradio.ru/api/track.php?channel=' + this.currentChannel );
-		xhr.responseType = 'json';
-		xhr.onloadend = function(){
-			if (xhr.readyState === 4 && xhr.status === 200){
-				callback(xhr.response);
-				console.log(xhr.response);
-			}
-		};
-			xhr.send();
-	},
+    request: function(callback){
+        var xhr = new XMLHttpRequest(),
+                self = this;
+        xhr.open('GET', 'http://sectorradio.ru/api/track.php?channel=' + this.currentChannel );
+        xhr.responseType = 'json';
+        xhr.onloadend = function(){
+            if (xhr.readyState === 4 && xhr.status === 200){
+                callback(xhr.response);
+            }
+        };
+            xhr.send();
+    },
 
-	handleResponse: function(){
-		SECTOR.updateState.request(function(response){
-			var trackNameContainer = document.getElementsByClassName('player__trackname')[0],
-					artistNameContainer = document.getElementsByClassName('player__title')[0],
-					timeTillTrackEnd = (response.length - (response.timestamp - response.serverTime))*1000; //milliseconds
+    handleResponse: function(){
+        var xhrUrl = 'http://sectorradio.ru/api/track.php?channel=' + this.currentChannel,
+            self = this;
+        SECTOR.requestHandler.handleRequest(xhrUrl, function(response){
+            var trackNameContainer = document.getElementsByClassName('player__trackname')[0],
+                    artistNameContainer = document.getElementsByClassName('player__title')[0],
+                    timeTillTrackEnd = (response.length - (response.timestamp - response.serverTime))*1000; //milliseconds
+            //On response let's fill track name and artist name
+            trackNameContainer.textContent = response.title;
+            artistNameContainer.textContent = response.artist;
+        
+        //TODO разобраться с отрицательным временем до конца трека
 
-			//On response let's fill track name and artist name
-			trackNameContainer.textContent = response.title;
-			artistNameContainer.textContent = response.artist;
-		
-		//TODO разобраться с отрицательным временем до конца трека
-		if(timeTillTrackEnd<0){
-			timeTillTrackEnd = 3000;
-			setTimeout(SECTOR.updateState.handleResponse, timeTillTrackEnd);
-		} else {
-			setTimeout(SECTOR.updateState.handleResponse, timeTillTrackEnd);
-		}
-			console.log('Time till track end: ' + timeTillTrackEnd);
-		})
-	},
+    
+        if(timeTillTrackEnd<0){
+            timeTillTrackEnd = 3000;
+        }
+            setTimeout(self.handleResponse.bind(self), timeTillTrackEnd);
+        })
+    },
 
-	init: function (){
-		this.handleResponse();
-	}
+    init: function (){
+        this.handleResponse();
+    }
 };
 
 SECTOR.updateState.init();
