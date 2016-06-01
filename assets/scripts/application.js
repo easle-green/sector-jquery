02615270cefa582cce0cd7442df215f34b5c60af
Alexey Solovev <halakul@live.ru>
2016-06-01 13:48:34 +0300

    forgotten files

--- assets/scripts/application.js
+++ assets/scripts/application.js
@@ -6,38 +6,18 @@
 //require('./common');
 //require.context('./components', true, /\.js$/)([]);
 
-
-
-
-const sector = require('./conf');
-
-sector.modules.forEach((module)=> {
-  connect(module);
-});
-
-function connect(module) {
-  let path = './components/' + module + '/';
-  require(path + 'Model')(sector);
-  require(path + 'Ctrl')(sector);
-  // require(path + 'style');
-  require(path + 'Component')(
-    sector,
-    require(path + 'template.html')
-  );
-};
-
 var SECTOR = {};
 SECTOR.api = require('./components/api')();
 var progress = require('./components/progressBar')(SECTOR);
 var status = require('./components/updateStatus')(SECTOR);
-// var program = require('./components/updateProgram')(SECTOR);
+var program = require('./components/updateProgram')(SECTOR);
 var slider = require('./components/volumeSlider');
-//
-//
+
+
 require('../styles/application');
-//
+
 SECTOR.api.init();
-// program.init();
+program.init();
 status.init();
 progress.init();
 slider();
---
+++ assets/scripts/conf.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const angular = require('angular');
+
+const sector = angular.module('sector',[]);
+
+sector.modules = [
+  'bitrate',
+  'blocks',
+  'equalizer'
+  // 'player',
+  // 'sidebar',
+  // 'tab'
+];
+
+sector.value('DEFAULTS', {
+  bitrate: 160,
+  tab: 0,
+  rates: [
+    56,
+    160
+  ],
+  equalizer: [
+    {freq: "0.2", level: 100, active: 0},
+    {freq: "1", level: 90, active: 0},
+    {freq: "3", level: 70, active: 0},
+    {freq: "5", level: 60, active: 0},
+    {freq: "8", level: 70, active: 0},
+    {freq: "6", level: 70, active: 0},
+    {freq: "9", level: 60, active: 0},
+    {freq: "10", level: 80, active: 56},
+    {freq: "11", level: 60, active: 56},
+    {freq: "12", level: 50, active: 56},
+    {freq: "13", level: 60, active: 56},
+    {freq: "15", level: 70, active: 160},
+    {freq: "16", level: 60, active: 160},
+    {freq: "17", level: 80, active: 160},
+    {freq: "18", level: 80, active: 160},
+    {freq: "20", level: 100, active: 256}
+  ]
+});
+
+module.exports = sector;
+
