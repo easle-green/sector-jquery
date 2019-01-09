f3e5156f15847de33ae406e13010e1ad18c5bb5e
Ksusha132 <orlovaksusha@mail.ru>
2019-01-10 01:00:46 +0300

    fixed ruRu property file renamed property file added function which determine the browser locale fixed active problem
    worked

--- assets/scripts/components/langSwitcher.js
+++ assets/scripts/components/langSwitcher.js
@@ -1,7 +1,7 @@
 'use strict';
 const localizedProperty = {
-    ruRu: require('../languages/ruRU.json'),
-    enUs: require('../languages/enUS.json')
+    'ru-RU': require('../languages/ru-RU.json'),
+    'en-US': require('../languages/en-US.json')
 };
 
 
@@ -11,42 +11,61 @@ function switcher(SECTOR) {
     return {
 
         byId: 'lang-switcher',
-        currentLanguage: 'ruRu',
-        defaultLanguage: 'ruRu',
+        currentLanguage: 'ru-RU',
+        defaultLanguage: 'ru-RU',
+        browserLanguage: '',
 
         init: function () {
+            this.languageDeterminater();
+            this.languageProcessor();
             this.onClick = this.onClick.bind(this);
             this.languageSwitcher();
-            this.languageProcessor();
+        },
+
+        languageDeterminater: function () {
+            debugger;
+            this.browserLanguage = window.navigator.language;
+            if (this.browserLanguage !== null) {
+                this.currentLanguage = this.browserLanguage;
+                const elem = document.querySelector(`[data-id='${this.browserLanguage}']`);
+                elem.className += ' active';
+            }
         },
 
         languageProcessor() {
-            debugger;
+
             let foundProperty = localizedProperty[this.currentLanguage];
 
             if (foundProperty === null) {
                 this.currentLanguage = this.defaultLanguage;
+                foundProperty = localizedProperty[this.currentLanguage];
             }
 
             for (let translation in foundProperty) {
-                const elem = document.querySelector(`[data-lang='${translation}']`); // found dom element by key of json
+                const elem = document.querySelector(`[data-lang='${translation}']`);
                 if (elem !== null) {
-                    elem.innerHTML = foundProperty[translation]; // Change the HTML content of a <h2 class="logo__slogan-about" data-lang="head"></h2>
+                    elem.innerHTML = foundProperty[translation];
                 }
             }
         },
 
         languageSwitcher() {
-            document.getElementById(this.byId).addEventListener("click", this.onClick); // when other language is clicked
+            document.getElementById(this.byId).addEventListener("click", this.onClick);
         },
 
         onClick(event) {
-            debugger;
-            if (event.target.classList.contains('active')) { // if chousen language the same as current
+            if (event.target.classList.contains('active')) {
                 return;
             }
             if (event.target.tagName.toLocaleLowerCase() === 'a') {
-                this.currentLanguage = event.target.getAttribute('data-id'); // enUs
+                this.currentLanguage = event.target.getAttribute('data-id');
+
+                let lis = document.querySelectorAll('#lang-switcher li a');
+
+                for (let i = 0; i < lis.length; i++) {
+                    lis[i].className = 'lang__icon';
+                }
+                event.target.className += ' active';
                 this.languageProcessor()
             }
         },
